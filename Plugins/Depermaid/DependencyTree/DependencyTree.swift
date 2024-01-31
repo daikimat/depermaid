2230a5c9b11bdfae9125e82affa93b3735ca696b
Daiki Matsumoto <daiki-matsumoto@cybozu.co.jp>
2024-01-31 23:50:29 +0900

    Add filterTransitiveDependencies()

--- Plugins/Depermaid/DependencyTree/DependencyTree.swift
+++ Plugins/Depermaid/DependencyTree/DependencyTree.swift
@@ -16,4 +16,59 @@ struct DependencyTree {
             dependencies[source]?.insert(destination)
         }
     }
+    
+    func filterTransitiveDependencies() -> DependencyTree {
+        var filteredDependencies: [Node: Set<Node>] = [:]
+        
+        for (parentNode, childrenNode) in dependencies {
+            filteredDependencies[parentNode] = Set<Node>()
+            var filterdChildrenNodes = childrenNode
+            for childNode in filterdChildrenNodes {
+                if childNode == parentNode {
+                    continue
+                }
+                var otherChildNodes = filterdChildrenNodes.subtracting([childNode])
+                for otherNode in otherChildNodes {
+                    if find(of: otherNode, startingFrom: childNode) {
+                        filterdChildrenNodes.remove(otherNode)
+                        continue
+                    }
+                }
+                filteredDependencies[parentNode] = filterdChildrenNodes
+            }
+        }
+        
+        return DependencyTree(dependencies: filteredDependencies)
+    }
+    
+    private func find(of target: Node, startingFrom source: Node) -> Bool {
+        guard let dependenciesOfSource = dependencies[source] else {
+            return false
+        }
+        
+        var visitedNodes: Set<Node> = []
+        var depthQueue: [Node] = []
+        
+        depthQueue.append(source)
+        visitedNodes.insert(source)
+        
+        while !depthQueue.isEmpty {
+            let current = depthQueue.removeFirst()
+            
+            if current == target {
+                return true
+            }
+            
+            if let dependenciesOfCurrent = dependencies[current] {
+                for nextNode in dependenciesOfCurrent {
+                    if !visitedNodes.contains(nextNode) {
+                        visitedNodes.insert(nextNode)
+                        depthQueue.append(nextNode)
+                    }
+                }
+            }
+        }
+        
+        return false
+    }
 }
